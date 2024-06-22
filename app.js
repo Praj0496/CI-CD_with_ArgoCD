b0689d1ffd29b9956e8d72d7ac0557fc4d825194
unknown <prajwalrao0404@gmail.com>
2024-06-22 12:34:09 +0530

    app ile updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end('Hello, this is update for recording and new file');
+    res.end('Hello, this update from ArgoCD project');
 }
 
 const server = http.createServer(requestListener);
